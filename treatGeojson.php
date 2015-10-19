1f2d97b57ea57269ce4ac7fa50c71b4993eccc49
ben-tei <benjaminteisseyre@yahoo.fr>
2015-10-19 16:57:21 +0200

    Update treatGeojson.php
    - Nouvel affichage des diplômés
    - Nouveau calcul de la promotion

--- treatGeojson.php
+++ treatGeojson.php
@@ -48,30 +48,30 @@ foreach ($decoded["polyMap"] as $stagiaire) {
 
     // Traitement Section du stagiaire
     $stagiaire[4] = preg_replace("#[0-9]([a-z]*)#", "", $stagiaire[4]);
-    if ($stagiaire[5]<3) {
+    if ($stagiaire[5] < 3) {
       // Si l'année est 1 ou 2, alors c'est forcément un PeiP (merci Dylan !)
       $stagiaire[4] = "PeiP";
     }
-		else if ($stagiaire[4] == "STIA" || $stagiaire[4] == "STIAEC") {
-			// STIA n'existe plus ... à la place : GBA
-			$stagiaire[4] = "GBA";
-		}
-		else if ($stagiaire[4] == "INFO") {
-			$stagiaire[4] = "IG";
-		}
-		else if ($stagiaire[4] == "EII" || $stagiaire[4] == "ERII" || $stagiaire[4] == "MEAEC") {
-			$stagiaire[4] = "MEA";
-		}
-		else if ($stagiaire[4] == "MATEC") {
-			$stagiaire[4] = "MAT";
-		}
-		else if ($stagiaire[4] == "STEEC") {
-			$stagiaire[4] = "STE";
-		}
-    $stagiaireTraite["properties"]["section"] = $stagiaire[4];
+	else if ($stagiaire[4] == "STIA" || $stagiaire[4] == "STIAEC") {
+		// STIA n'existe plus ... à la place : GBA
+		$stagiaire[4] = "GBA";
+	}
+	else if ($stagiaire[4] == "INFO") {
+		$stagiaire[4] = "IG";
+	}
+	else if ($stagiaire[4] == "EII" || $stagiaire[4] == "ERII" || $stagiaire[4] == "MEAEC") {
+		$stagiaire[4] = "MEA";
+	}
+	else if ($stagiaire[4] == "MATEC") {
+		$stagiaire[4] = "MAT";
+	}
+	else if ($stagiaire[4] == "STEEC") {
+		$stagiaire[4] = "STE";
+	}
+	$stagiaireTraite["properties"]["section"] = $stagiaire[4];
 
-		// Traitement Promotion du stagiaire (!! ne pas faire confiance à cette valeur !!)
-		$stagiaireTraite["properties"]["promo"] = $stagiaire[3];
+	// Traitement Promotion du stagiaire (!! ne pas faire confiance à cette valeur !!)
+	//$stagiaireTraite["properties"]["promo"] = $stagiaire[3];
 
     // Traitement Dates du stage
     $stagiaireTraite["properties"]["date_deb"] = strtotime($stagiaire[7]);
@@ -81,7 +81,17 @@ foreach ($decoded["polyMap"] as $stagiaire) {
     $dateDebutStage = strftime("%A %d %B %Y", $stagiaireTraite["properties"]["date_deb"]);
     $dateFinStage = strftime("%A %d %B %Y", $stagiaireTraite["properties"]["date_fin"]);
     $texteDates = utf8_encode("Du {$dateDebutStage}<br />Au {$dateFinStage}");
-    $nom = "<b>Étudiant {$stagiaire[4]} de {$stagiaire[5]}è année</b><br />Promotion {$stagiaire[3]}<br /><br />{$texteDates}";
+    if($stagiaire[5] > 5)
+    {
+    	$nom = "<b>Diplomé {$stagiaire[4]}</b><br />";
+    }
+    else
+    {
+    	$nom = "<b>Étudiant {$stagiaire[4]} de {$stagiaire[5]}è année</b><br />";
+    }
+    // Traitement Promotion du stagiaire = Année Stage + 5 ans de formation - Année étudiant lors du stage
+    $stagiaire[3] = intval(substr($dateDebutStage, -4)) + 5 - intval($stagiaire[5]);
+    $nom = $nom . "Promotion {$stagiaire[3]}<br /><br />{$texteDates}";
     $stagiaireTraite["properties"]["nom"] = htmlentities($nom);
 
     // Transformation en coordonnées GPS
@@ -89,7 +99,7 @@ foreach ($decoded["polyMap"] as $stagiaire) {
     $adressePourUrl = urlencode($concatAdresse);
     $adresseGoogle = "http://maps.google.com/maps/api/geocode/json?address={$adressePourUrl}&sensor=false";
 
-    if ($i<560) {
+    if ($i < 560) {
       $moissonGoogle = file_get_contents_utf8($adresseGoogle);
       $decodeGoogle = json_decode($moissonGoogle, true);
       $stagiaireTraite["geometry"]["type"] = "Point";
