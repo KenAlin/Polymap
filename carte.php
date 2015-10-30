c9a9b5224537a1cf15fdba7cf1fb7db69028778c
KenAlin <kek.serv@gmail.com>
2015-10-30 01:26:07 +0100

    jquery ui : ajout du support tactile

--- carte.php
+++ carte.php
@@ -19,6 +19,7 @@
   <link rel="stylesheet" href="scripting/leaflet.css" />
   <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script src="scripting/touch.js"></script>
   <link rel="stylesheet" href="polymap.css" />
   <link rel="stylesheet" href="scripting/cluster.css" />
   <script src="scripting/filtres.js"></script>
